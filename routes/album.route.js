0075b45e4a14b2485e24d2ef039ca90d9a09b37f
cvlnpsg987 <cvlnpsg987@gmail.com>
2025-04-21 15:52:14 +0000

    Album controllers added

--- routes/album.route.js
+++ routes/album.route.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
+import { getAlbumById, getAllAlbums } from "../controllers/album.controller.js";
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send('Album route with GET method');
-});
+router.get('/', getAllAlbums);
+router.get('/:id', getAlbumById);
 
 export default router;
